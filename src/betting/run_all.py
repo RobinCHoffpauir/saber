8f5ce63dafdd4e5dde3173c60d1e65efe34e0777
robinchoffpauir <hoffpauirrobin@gmail.com>
2024-04-29 20:52:45 -0500

    create run_all.py

---
+++ src/betting/run_all.py
@@ -0,0 +1,18 @@
+
+import scores
+import odds
+import covers
+import warnings
+warnings.filterwarnings("ignore")
+
+#code for ignore warnings
+
+
+def main():
+    odds.main()
+    covers.main()
+    scores.main()
+    print('all scripts executed: Completed!')
+
+if __name__ == "__main__":
+    main()
